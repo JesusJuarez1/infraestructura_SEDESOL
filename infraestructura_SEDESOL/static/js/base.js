b84356bdf89a44924336f9967b35d037d756a0dd
Jesus Juarez <pasillasjesus9@gmail.com>
2023-10-07 17:41:38 -0600

    Templates login y base responsive

--- infraestructura_SEDESOL/static/js/base.js
+++ infraestructura_SEDESOL/static/js/base.js
@@ -5,3 +5,10 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+const btnToggle = document.querySelector('.toggle-btn');
+
+btnToggle.addEventListener('click', function () {
+  document.getElementById('sidebar').classList.toggle('active');
+  console.log(document.getElementById('sidebar'))
+});
--- infraestructura_SEDESOL/static/js/dropdown.js
+++ infraestructura_SEDESOL/static/js/dropdown.js
@@ -1,20 +1,58 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var userDropdown = document.getElementById('userDropdown');
-    var adminButton = document.getElementById('adminButton');
-    var logoutButton = document.getElementById('logoutButton');
-    var arrow = document.querySelector('.arrow');
+    var userDropdownDesktop = document.getElementById('userDropdownDesktop');
+    var userDropdownMobile = document.getElementById('userDropdownMobile');
     
-    if (userDropdown && adminButton && logoutButton && arrow) {
-        userDropdown.addEventListener('click', function() {
-            var isOpen = arrow.innerHTML === '▼'; // Verifica si la flecha hacia abajo está presente
-            arrow.innerHTML = isOpen ? '▲' : '▼'; // Cambia la flecha hacia arriba o hacia abajo
-        });
-        userDropdown.addEventListener('click', function() {
-            var adminButtonDisplay = window.getComputedStyle(adminButton).getPropertyValue('display');
-            var logoutButtonDisplay = window.getComputedStyle(logoutButton).getPropertyValue('display');
+    if (userDropdownDesktop) {
+        userDropdownDesktop.addEventListener('click', function() {
+            var arrow = userDropdownDesktop.querySelector('.arrow');
             
-            adminButton.style.display = adminButtonDisplay === 'none' ? 'block' : 'none';
-            logoutButton.style.display = logoutButtonDisplay === 'none' ? 'block' : 'none';
+            if (arrow) {
+                if (arrow.classList.contains('fa-chevron-down')) {
+                    arrow.classList.remove('fa-chevron-down');
+                    arrow.classList.add('fa-chevron-up');
+                } else {
+                    arrow.classList.remove('fa-chevron-up');
+                    arrow.classList.add('fa-chevron-down');
+                }
+            }
+
+            var adminButton = document.getElementById('adminButton');
+            var logoutButton = document.getElementById('logoutButton');
+            
+            if (adminButton && logoutButton) {
+                var adminButtonDisplay = window.getComputedStyle(adminButton).getPropertyValue('display');
+                var logoutButtonDisplay = window.getComputedStyle(logoutButton).getPropertyValue('display');
+                
+                adminButton.style.display = adminButtonDisplay === 'none' ? 'block' : 'none';
+                logoutButton.style.display = logoutButtonDisplay === 'none' ? 'block' : 'none';
+            }
+        });
+    }
+
+    if (userDropdownMobile) {
+        userDropdownMobile.addEventListener('click', function() {
+            var arrow = userDropdownMobile.querySelector('.arrow');
+            
+            if (arrow) {
+                if (arrow.classList.contains('fa-chevron-down')) {
+                    arrow.classList.remove('fa-chevron-down');
+                    arrow.classList.add('fa-chevron-up');
+                } else {
+                    arrow.classList.remove('fa-chevron-up');
+                    arrow.classList.add('fa-chevron-down');
+                }
+            }
+
+            var adminButton = document.getElementById('adminButton-mobile');
+            var logoutButton = document.getElementById('logoutButton-mobile');
+            
+            if (adminButton && logoutButton) {
+                var adminButtonDisplay = window.getComputedStyle(adminButton).getPropertyValue('display');
+                var logoutButtonDisplay = window.getComputedStyle(logoutButton).getPropertyValue('display');
+                
+                adminButton.style.display = adminButtonDisplay === 'none' ? 'block' : 'none';
+                logoutButton.style.display = logoutButtonDisplay === 'none' ? 'block' : 'none';
+            }
         });
     }
 });
